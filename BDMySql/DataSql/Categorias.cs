af4f383e520464de3718295ecacb3ff5feddc134
tech@luegopago.com <Siste2020*>
2020-10-02 12:47:33 -0500

    add db sqlserver

---
+++ BDMySql/DataSql/Categorias.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BDMySql.DataSql
+{
+    public partial class Categorias
+    {
+        public string Nivel { get; set; }
+        public string CategId { get; set; }
+        public string CategName { get; set; }
+        public string CategSlug { get; set; }
+        public string ParentId { get; set; }
+        public string IdAttribute { get; set; }
+        public string NameAttribute { get; set; }
+        public string IdValue { get; set; }
+        public string NameValue { get; set; }
+    }
+}
---
+++ BDMySql/DataSql/sellerContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BDMySql.DataSql
+{
+    public partial class sellerContext : DbContext
+    {
+        public sellerContext()
+        {
+        }
+
+        public sellerContext(DbContextOptions<sellerContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categorias> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=190.146.25.236;Initial Catalog=seller;User ID=sa;Password=Dna9201,,,;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categorias>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.CategId)
+                    .HasColumnName("Categ Id")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.CategName)
+                    .HasColumnName("Categ Name")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.CategSlug)
+                    .HasColumnName("Categ Slug")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.IdAttribute)
+                    .HasColumnName("Id_Attribute")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.IdValue)
+                    .HasColumnName("ID_Value")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.NameAttribute)
+                    .HasColumnName("Name_Attribute")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.NameValue)
+                    .HasColumnName("Name_Value")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.Nivel).HasMaxLength(255);
+
+                entity.Property(e => e.ParentId)
+                    .HasColumnName("Parent Id")
+                    .HasMaxLength(255);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
