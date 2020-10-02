d7b4e5ecb14b3f46f3ec4a8143cabd95c3e4a03b
tech@luegopago.com <Siste2020*>
2020-10-02 12:49:06 -0500

    sql server

--- BDMySql/DataSql/sellerContext.cs
+++ BDMySql/DataSql/sellerContext.cs
@@ -21,7 +21,6 @@ namespace BDMySql.DataSql
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Data Source=190.146.25.236;Initial Catalog=seller;User ID=sa;Password=Dna9201,,,;");
             }
         }
